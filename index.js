af29a83197e7ba737fa13c35a9a5f2776bc40c7b
devlopingandroid <yashgoel15119@gmail.com>
2026-06-18 21:08:50 +0530

    Fix github route

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/about',(req,res)=>{
-    res.send("this is about page");
-})
+app.get('/about', (req, res) => {
+  res.send('this is about page');
+});
 
-app.get('/contact',(req,res)=>{
-    res.send("this is contact page");
-})
+app.get('/contact', (req, res) => {
+  res.send('this is contact page');
+});
 
-app.get('/github',(req,res)=>{
-    res.send(githubData);
+app.get('/github', (req, res) => {
+  res.send('Github data');
 });
 
 app.listen(port, () => {
